587fc49be87586071275897ba31871c63a7cf571
Bivol Mihail <mihail.bivol@iongroup.com>
2025-11-19 11:03:41 +0200

    lesson 4: key features of Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,18 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'beginners';
+  data = {
+    title: '<h1>beginners</h1><script>alert("Attack")</script>',
+  };
+
+  onLogoClicked() {
+    alert('Hello World');
+  }
+
+  onKeyUp(newTitle: string) {
+    this.data.title = newTitle;
+  }
 }

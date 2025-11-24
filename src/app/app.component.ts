002b1f73d81ac7d1d0e738f4435eba46eecbbc16
Bivol Mihail <mihail.bivol@iongroup.com>
2025-11-24 11:02:06 +0200

    Lesson 32: Async pipe and alias asignment of an asynchronous array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DatePipe, TitleCasePipe } from '@angular/common';
+import { DatePipe, TitleCasePipe, AsyncPipe } from '@angular/common';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CourseCardComponent, DatePipe, TitleCasePipe],
+  imports: [
+    RouterOutlet,
+    CourseCardComponent,
+    DatePipe,
+    TitleCasePipe,
+    AsyncPipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title: string = 'app Title with irregular Capitalization';
+  courses$?: Observable<Course[]>;
   courses?: Course[];
   startDate: Date = new Date(2025, 0, 1);
 
@@ -22,10 +30,9 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     const params: HttpParams = new HttpParams()
-      .set("page", "1")
-      .set("pageSize", "5");
-    this.http.get<Course[]>('/api/courses', {params})
-      .subscribe((courses) => this.courses = courses);
+      .set('page', '1')
+      .set('pageSize', '5');
+    this.courses$ = this.http.get<Course[]>('/api/courses', { params });
   }
 
   onCourseSelected(course: Course): void {

cc4317adf6ce74b408647d0363fc52ef0c827f4a
Bivol Mihail <mihail.bivol@iongroup.com>
2025-11-24 10:52:56 +0200

    lesson 31: angular HttpClient, calss with request params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatePipe, TitleCasePipe } from '@angular/common';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
@@ -12,10 +13,20 @@ import { Course } from './model/course';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'app Title with irregular Capitalization';
-  courses: Course[] = COURSES;
+  courses?: Course[];
   startDate: Date = new Date(2025, 0, 1);
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const params: HttpParams = new HttpParams()
+      .set("page", "1")
+      .set("pageSize", "5");
+    this.http.get<Course[]>('/api/courses', {params})
+      .subscribe((courses) => this.courses = courses);
+  }
 
   onCourseSelected(course: Course): void {
     console.log(`Course clicked ${course.description}`);
@@ -23,5 +34,5 @@ export class AppComponent {
 
   trackCourse(id: number, course: Course) {
     return course.id;
-  } 
+  }
 }
